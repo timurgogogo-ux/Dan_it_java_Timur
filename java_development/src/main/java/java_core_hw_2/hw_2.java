282ef48e96e82e126edb26bdeda0df2f0a61a732
Fizruk <timurgogogo@gmail.com>
2025-10-10 18:14:48 +0300

    homework 2

---
+++ java_development/src/main/java/java_core_hw_2/hw_2.java
@@ -0,0 +1,27 @@
+package java_core_hw_2;
+
+public class hw_2 {
+    public static void main(String[] args) {
+        System.out.println("gryffindor");
+        System.out.println("slithering");
+        System.out.println("Hafelpuff");
+        System.out.println("Ravenclaw");
+        int gryffindor = 482;
+        int slithering = 472;
+        int Hafelpuff = 352;
+        int Ravenclaw = 426;
+
+        gryffindor = 482 - 481;
+
+        slithering = 472 - 470;
+
+        Ravenclaw = 426 - 423;
+
+        Hafelpuff = 352 - 348;
+
+        System.out.println(gryffindor);
+        System.out.println(slithering);
+        System.out.println(Ravenclaw);
+        System.out.println(Hafelpuff);
+    }
+}
