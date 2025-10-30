2af340f73f9401f9cb67465d761c3befee1eb3f4
Fizruk <timurgogogo@gmail.com>
2025-10-30 19:10:30 +0200

    hw_4

---
+++ java_development/src/main/java/Timur/Program.java
@@ -0,0 +1,39 @@
+package Timur;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Person person = new Person( "Timur", "Shever", 40);
+
+
+        System.out.println(person);
+        System.out.println(person.getName());
+        person.setName("tratra");
+        System.out.println(person.getSurname());
+        person.setSurname("bbbbbbb");
+        System.out.println(person.getAge());
+        person.setAge(-33);
+
+        System.out.println(person);
+
+
+
+
+
+
+   //     String name = Sweta.name;
+    //    String surname = Sweta.surname;
+     //   int age = Sweta.age;
+
+//        System.out.println(" The person is:" + name +" " + surname +" and he is " + age + " years old");
+
+
+
+
+
+    }
+
+
+    }
+
+
